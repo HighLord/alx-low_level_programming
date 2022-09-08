6986473ac645ddb8f311a65f160b85c3ce7c1267
“HighLord” <1stempirelord@gmail.com>
2022-09-08 15:55:09 -0700

    c programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <studio.h>
+
+//function main initalizes  program execution
+int main(void)
+{
+	printf( "\"Programming is like building a multilingual puzzle\n");
+	return 0;
+}
+//returning 0 means success
