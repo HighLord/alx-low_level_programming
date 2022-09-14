d3c2af57c68427394f45e57307890c07c44d3ccf
“HighLord” <1stempirelord@gmail.com>
2022-09-13 17:41:40 -0700

    changes

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
-
 /**
- * print_times_table - prints times table
- *@n: is intiger variable for print times table
- *
- * Return: Always 0.
- */
-int main()
+* main - print thesum of all multiples of 3 or 5 below 1024
+*
+*Return: 0
+*/
+
+int main(void)
 {
-    long unsigned int i,sum=0;
-    clrscr();
-    for(i=0;i<1000;i++)
-    {
-        if((i%5==0)||(i%3==0))
-        {
-            sum=sum+i;
-        }
-    }
-    printf("%ld\n",sum);
-    getchar();
-    return 0;
+	int i, n, m, sum;
+
+	sum = 0;
+	for (i = 0; i < 1024; i++)
+	{
+		n = i % 3;
+		m = i % 5;
+		if (n == 0 || m == 0)
+			sum = sum + i;
+	}
+	printf("%d\n", sum);
+	return (0);
 }
