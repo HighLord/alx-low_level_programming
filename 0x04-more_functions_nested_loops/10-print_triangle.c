530347144aa41e01e9160a6af7f50d8e2b62f9b4
HighLord <1stempirelord@gmail.com>
2022-09-16 00:19:01 +0100

    Update 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,25 +10,26 @@
 
 void print_triangle(int size)
 {
-int i, j;
+	int i, j;
 
-for (i = size; i > 0; i--)
-{
-	for (j = 0; j <= size; j++)
+	if (size > 0)
 	{
-		if (i > j)
+		for (i = 1; i <= size; i++)
 		{
-			if (j == 0)
+			for (j = 1; j <= size - i; j++)
 			{
+				_putchar(32);
 			}
-			else
-			_putchar(' ');
+
+			for (j = 1; j <= i; j++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
 		}
-		else
-		_putchar('#');
 	}
-_putchar('\n');
-}
-if (size <= 0)
-_putchar('\n');
+	else
+	{
+		_putchar('\n');
+	}
 }
