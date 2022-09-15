8d11229308e7180149c009d42c28e1c3eec003e4
“HighLord” <1stempirelord@gmail.com>
2022-09-15 16:35:14 -0700

    learning c programming

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,53 +1,25 @@
 #include "main.h"
-void print_negative(int n);
-void print_positive(int n);
-void print_zero(int n);
 
 /**
  * print_number - prints an integer
- * @n: integer to print
+ * @n: integer to be printed
  */
 void print_number(int n)
 {
-	print_zero(n);
-	if (n < 0)
-		print_negative(n);
-	else if (n > 0)
-		print_positive(n);
-}
+unsigned int b;
 
-/**
- * print_negative - transform negative to positive, then calls print_positive
- * @n: integer to transform
- */
-void print_negative(int n)
+if (n < 0)
 {
-	_putchar(45);
-	n *= -1;
-	print_positive(n);
+b = -n;
+_putchar('-');
 }
-
-/**
- * print_positive - prints positive integer
- * @n: integer to print
- */
-void print_positive(int n)
+else
 {
-	if (n != 0)
-	{
-		print_positive(n / 10);
-		_putchar((n % 10) + '0');
-	}
+b = n;
 }
-
-/**
- * print_zero - if the integer is equal to 0 prints 0
- * @n: integer to print
- */
-void print_zero(int n)
+if (b / 10)
 {
-	if (n == 0)
-	{
-		_putchar('0');
-	}
+print_number(b / 10);
+}
+_putchar((b % 10) + '0');
 }
