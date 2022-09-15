aba51ee4ef77f9dc24959261e419e02f56482671
HighLord <1stempirelord@gmail.com>
2022-09-16 00:15:35 +0100

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,19 +1,18 @@
 #include "main.h"
 
 /**
- * print_numbers - Entry point
+ * print_numbers - prints the numbers, from 0 to 9
  *
- * Return: Always 0 (Success)
+ * Return: no return is a void
  */
-
 void print_numbers(void)
 {
-int a = 0;
+	int i;
 
-while (a < 10)
-{
-_putchar(a + '0');
-a++;
-}
-_putchar('\n');
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
 }
